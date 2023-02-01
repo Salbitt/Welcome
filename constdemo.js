baa534098a686dc14b6e1629a136b953ad9d6269
Salbitt <95120414+Salbitt@users.noreply.github.com>
2023-02-01 10:02:14 +0530

    Delete constdemo.js

--- constdemo.js
+++
@@ -1,8 +0,0 @@
-const pi = 3.142;
-var maxr = 52;
-var steps = 12;
-
-for(var radius = 1;radius<=maxr;radius+=steps)
-{
-    console.log('Radius: '+radius+" cm ; Area of circle: "+(pi*radius*radius)+" cm2 ; Perimeter: "+2*pi*radius+" cm");
-}
